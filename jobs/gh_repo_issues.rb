d79e6f6f611e100361707e0d4eb4d2616ada5a35
Jamie <jamiestobbs92@gmail.com>
2013-05-28 11:10:31 +0800

    cycle issue list implemented (some bugs to hammer out)

--- jobs/gh_repo_issues.rb
+++ jobs/gh_repo_issues.rb
@@ -7,51 +7,52 @@ require 'uri'
 # ------
 github_username = 'wiredcraft'
 
-# Issues for these repositories
-repo_name = ['embarq', 'gwilcher']
+# Last issue for this repo 
+repo_names = ['embarq', 'gwilcher', 'hnshanghai']
 
-# show this many issues
+# show this many issues per repo
 max_length = 1
 
 # order by (false for default github)
 ordered = false
 
+# list repos
+# /orgs/wiredcraft/repos
+
+# list issues for repo
+# "/repos/#{github_username}/#{repo_name}/issues"
+
 # curl -H "Authorization: token cf993c25daf7d73be73023cc088636bf930d8a0c" \https://api.github.com/path/to/info
 
 SCHEDULER.every '3m', :first_in => 0 do |job|
-  rpo = repo_name[0]
-  
-  uri = uri = URI.parse("https://api.github.com/repos/#{github_username}/#{rpo}/issues")
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  headers = { "Authorization" => "token cf993c25daf7d73be73023cc088636bf930d8a0c" }
-  request = Net::HTTP::Get.new(uri.request_uri, headers)
-  response = http.request(request)
+  repos_issues = Array.new
 
-  data = JSON.parse(response.body)
+  i = 0
+  while i < repo_names.length
+    rpo = repo_names[i]
 
-  if data
-    data.map! do |repo|
-      { title: "#{rpo}", issue: repo['title'], assignee: repo['user']['login'], comment: repo['body'] }
+    uri = uri = URI.parse("https://api.github.com/repos/#{github_username}/#{rpo}/issues")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    headers = { "Authorization" => "token cf993c25daf7d73be73023cc088636bf930d8a0c" }
+    request = Net::HTTP::Get.new(uri.request_uri, headers)
+    response = http.request(request)
+    data = JSON.parse(response.body)
+
+
+    if data
+      data.each do |repo|
+        repos_issues.push({
+        title: "#{rpo}",
+        issue: repo['title'],
+        assignee: repo['user']['login'],
+        body: repo['body']
+      })
+      end
     end
 
-    send_event('gh_repo_issues', { comments: data })
+    i += 1
   end
 
-  # if response.code != "200"
-  #   puts "github api error (status-code: #{response.code})\n#{response.body}"
-  # else
-  #   repos_issues = Array.new
-  #   data.each do |repo|
-  #     repos_issues.push({
-  #     issue: repo['title'],
-  #     assignee: repo['user']['login'],
-  #     comment: repo['body']
-  #   })
-  #   end
-
-  #   send_event('gh_last_issue', { items: repos_issues.slice(0, max_length) })
-
-  # end
-
+  send_event('gh_repo_issues', { comments: repos_issues })
 end
