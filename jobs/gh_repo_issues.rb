1e8912d279e38fc84e86d08a0602d2cc2e9dab92
Jamie <jamiestobbs92@gmail.com>
2013-06-07 12:36:33 +0800

    updated api key

--- jobs/gh_repo_issues.rb
+++ jobs/gh_repo_issues.rb
@@ -34,7 +34,7 @@ SCHEDULER.every '3m', :first_in => 0 do |job|
     uri = uri = URI.parse("https://api.github.com/repos/#{github_username}/#{rpo}/issues")
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
-    headers = { "Authorization" => "token cf993c25daf7d73be73023cc088636bf930d8a0c" }
+    headers = { "Authorization" => "token c2ea629f0a4619abd039df7e07ecc5bcb7e57917" }
     request = Net::HTTP::Get.new(uri.request_uri, headers)
     response = http.request(request)
     data = JSON.parse(response.body)
