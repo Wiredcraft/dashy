957b03d50a8fd133e1a062ec5fbd91c75a81c183
Jamie <jamiestobbs92@gmail.com>
2013-08-20 15:39:58 +0800

    add & update working with new widget data structure

--- app/js/controllers/admin_controller.js
+++ app/js/controllers/admin_controller.js
@@ -29,26 +29,24 @@ angular.module('Dashboard.Admin', [])
                 "template": content.template,
                 "options": content.options
             });
-            $scope.content = {"type": "data"}
+            $scope.content = {"type": "data"};
             $scope.content.options = {};
         }
-
-        // $scope.template1Remove = function(index) {
-        //     $scope.template1.splice(index, 1);
-        // }
+        $scope.contentRemove = function(index) {
+            $scope.widget.content.splice(index, 1);
+        }
 
         // Add widget function
         // >>>>>>>>>>>>>>>>>>>
         $scope.addWidget = function(widget){
             // Check Source Value
             // >>>>>>>>>>>>>>>>>>
-            if(widget.config.source == undefined) {
+            if(widget.config.source == undefined || widget.config.source == 'none') {
                 $scope.widget.config.source = " ";
             }
 
             // POST to Database
             // >>>>>>>>>>>>>>>>
-            console.log($scope.widget)
             Admin.addWidget($scope.widget);
         };
 
@@ -57,8 +55,10 @@ angular.module('Dashboard.Admin', [])
 
 .controller('UpdateCtrl', ['$scope', '$http', '$location', 'Admin', 'Sources', 'Widgets',
     function($scope, $http, $location, Admin, Sources, Widgets) {
-        $scope.updateSource = false;
-        $scope.updateTemplates = false;
+        // Define content
+        // >>>>>>>>>>>>>>
+        $scope.content = {"type": "data"};
+        $scope.content.options = {};
 
         // Get widget ID
         // >>>>>>>>>>>>>
@@ -69,7 +69,7 @@ angular.module('Dashboard.Admin', [])
         // >>>>>>>>>>>>>>>>>>>>>>
         Widgets.getWidgetById(thisId).then(function(data) {
             $scope.widget = data;
-            $scope.thisTitle = data.content.title;
+            $scope.thisTitle = data.config.title;
         });
 
         // Get list of useable datasources
@@ -84,74 +84,29 @@ angular.module('Dashboard.Admin', [])
             $scope.dbWidgets = data[0];
         });
 
-        // Template Options
-        // >>>>>>>>>>>>>>>>
-        $scope.template1 = [];
-        $scope.template2 = [];
-
-        $scope.template1Add = function(template1) {
-            $scope.template1.push({"key": template1.key, "value": template1.value});
-            template1.key = "", template1.value = "";
+        // Content Functions
+        // >>>>>>>>>>>>>>>>>
+        $scope.contentAdd = function(content) {
+            $scope.widget.content.push({
+                "type": content.type,
+                "refresh": content.refresh,
+                "template": content.template,
+                "options": content.options
+            });
+            $scope.content = {"type": "data"};
+            $scope.content.options = {};
         }
-        $scope.template1Remove = function(index) {
-            $scope.template1.splice(index, 1);
-        }
-
-        $scope.template2Add = function(template2) {
-            $scope.template2.push({"key": template2.key, "value": template2.value});
-            template2.key = "", template2.value = "";
-        }
-        $scope.template2Remove = function(index) {
-            $scope.template2.splice(index, 1);
+        $scope.contentRemove = function(index) {
+            $scope.widget.content.splice(index, 1);
         }
 
         // Update Function
         // >>>>>>>>>>>>>>>
-        $scope.updateWidget = function(widget, options) {
-            var dir1 = {};
-            var dir2 = {};
-
+        $scope.updateWidget = function(widget) {
             // Check Source Value
             // >>>>>>>>>>>>>>>>>>
-            if(widget.config.source == undefined) {
+            if(widget.config.source == undefined || widget.config.source == 'none') {
                 widget.config.source = " ";
-            }
-
-            // Prepare Template Options
-            // >>>>>>>>>>>>>>>>>>>>>>>>
-            if ($scope.template1.length > 0) {
-                for(i = 0; i < $scope.template1.length; i++) {
-                    dir1[$scope.template1[i].key] = $scope.template1[i].value;
-                };                
-            }
-
-            if ($scope.template2.length > 0) {
-                for(i = 0; i < $scope.template2.length; i++) {
-                    dir2[$scope.template2[i].key] = $scope.template2[i].value;
-                };                
-            }
-
-            // Get current template names
-            // >>>>>>>>>>>>>>>>>>>>>>>>>>
-            var currentTemplates = Object.keys(widget.content.templates);
-
-            // Replace templates if new ones chosen
-            // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-            if (options !== undefined) {
-                if (options.template1 !== undefined && options.template1 !== 'undefined') {
-                    delete widget.content.templates[currentTemplates[0]];
-                    widget.content.templates[options.template1] = dir1;
-                }
-                if (options.template2 !== undefined && options.template2 !== 'undefined') {
-                    delete widget.content.templates[currentTemplates[1]];
-                    widget.content.templates[options.template2] = dir2;
-                }
-            }
-
-            // Remove the 2nd template if selected
-            // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-            if (options != undefined && options.remove != undefined && options.remove === true) {
-                delete widget.content.templates[currentTemplates[1]];
             }
 
             // PUT update to db
