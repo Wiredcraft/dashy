824467e6e8f211770441a039e6e14038605bb179
Jamie <jamiestobbs92@gmail.com>
2013-08-21 16:27:47 +0800

    commenting out modal code for time being

--- app/js/controllers/admin_controller.js
+++ app/js/controllers/admin_controller.js
@@ -121,22 +121,22 @@ angular.module('Dashboard.Admin', ['ui.bootstrap'])
     }
 ])
 
-.controller('ModalDemoCtrl', ['$scope', '$http', 'Admin', 'Sources', '$location',
-    function($scope, $http, Admin, Sources, $location) {
-        $scope.open = function() {
-            $scope.shouldBeOpen = true;
-        };
+// .controller('ModalDemoCtrl', ['$scope', '$http', 'Admin', 'Sources', '$location',
+//     function($scope, $http, Admin, Sources, $location) {
+//         $scope.open = function() {
+//             $scope.shouldBeOpen = true;
+//         };
 
-        $scope.close = function() {
-            $scope.closeMsg = 'I was closed at: ' + new Date();
-            $scope.shouldBeOpen = false;
-        }
+//         $scope.close = function() {
+//             $scope.closeMsg = 'I was closed at: ' + new Date();
+//             $scope.shouldBeOpen = false;
+//         }
 
-        $scope.items = ['item1', 'item2']
+//         $scope.items = ['item1', 'item2']
 
-        $scope.opts = {
-            backdropFade: true,
-            dialogFade: true
-        };
-    }
-])
+//         $scope.opts = {
+//             backdropFade: true,
+//             dialogFade: true
+//         };
+//     }
+// ])
