50ca75c9f03d706825714047f18cc66dff648bd7
Jamie <jamiestobbs92@gmail.com>
2013-09-10 17:58:34 +0800

    converting to feathers

---
+++ app/src/app/app.js
@@ -0,0 +1,63 @@
+// Declare app level module which depends on filters, and services
+angular.module('Dashboard', [
+    'templates-app',
+    'templates-common',
+
+    'Dashboard.Navbar',
+
+    // directives
+    'Dashboard.Tags',
+    'Dashboard.Charts',
+
+    // filters
+    'Dashboard.Filters',
+    'Dashboard.Formatters',
+
+    // utils
+    'Dashboard.Utils'
+]).config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'partials/layout.html',
+            controller: 'MainAppCtrl'
+        }).when('/add', {
+            templateUrl: 'partials/addwidget.html',
+            controller: 'AddCtrl'
+        }).when('/update', {
+            templateUrl: 'partials/updatewidget.html',
+            controller: 'UpdateCtrl'
+        }).otherwise({
+            redirectTo: '/404'
+        });
+    }
+])
+
+// Main controller
+.controller('MainAppCtrl', ['$scope', 'Widgets', '$rootScope', '$document',
+    function($scope, Widgets, $rootScope, $document) {
+        // Get widgets
+        Widgets.getWidgetList().then(function(data) {
+            $scope.widgets = data;
+        }, function(err) {
+            console.log(err);
+        });
+
+        // Press 'L' to un/lock dragging
+        $document.keydown(function(e) {
+            // console.log(e.keyCode);
+            if (e.keyCode === 76) {
+                if($rootScope.locked === false) {
+                    $rootScope.gridster.disable();
+                    $rootScope.locked = true;
+                } else if($rootScope.locked === true) {
+                    $rootScope.gridster.enable();
+                    $rootScope.locked = false;
+                }                
+            }
+        });
+
+    }
+])
+
+;
+
---
+++ app/src/app/navbar/navbar.js
@@ -0,0 +1,46 @@
+angular.module('Dashboard.Navbar', [
+
+])
+
+.config(function($routeProvider, $locationProvider) {
+        $routeProvider.when('/', {        
+            controller: 'NavCtrl',
+            templateUrl: 'navbar/navbar.tpl.html'
+        });
+    }
+})
+
+.controller('NavCtrl', ['$scope', '$rootScope', '$location',
+    function($scope, $rootScope, $location) {
+        // Modal Controls
+        $scope.opts = {
+            backdropFade: false,
+            dialogFade: true
+        };
+        $scope.open = function() {
+            $scope.shouldBeOpen = true;
+        };
+        $scope.close = function() {
+            $scope.shouldBeOpen = false;
+        };
+
+        // Lock / Unlock draggable
+        $scope.lock = function() {
+            $rootScope.gridster.disable();
+            $rootScope.locked = true;
+        };
+        $scope.unlock = function() {
+            $rootScope.gridster.enable();
+            $rootScope.locked = false;
+        };
+
+        // Manually refresh widget data.
+        // For now refreshes whole page.
+        $scope.refresh = function() {
+            document.location.reload(true);
+        }
+
+    }
+])
+
+;
