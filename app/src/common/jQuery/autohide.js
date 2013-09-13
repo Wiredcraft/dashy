d9f24dc8e4cecaecb4c888b094fa434804337eb9
Jamie <jamiestobbs92@gmail.com>
2013-09-13 11:11:40 +0800

    jQuery autohide navbar

---
+++ app/src/common/jQuery/autohide.js
@@ -0,0 +1,18 @@
+showDistance = 40;
+
+$(document).ready(function() {
+    $("#menu").hide();
+});
+
+function showNavBar() {
+    $("#menu").show('slow', 'linear');
+};
+
+$(document).mousemove(function(ev) {
+    var ev = ev || window.event;
+    if(ev.pageX <= showDistance) {
+        showNavBar()
+    } else if (ev.pageX > showDistance) {
+        $("#menu").hide('slow', 'linear');
+    }
+});
