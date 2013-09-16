1d80fb5c9e25d14b052ebcb1783f83784f7217d1
Jamie <jamiestobbs92@gmail.com>
2013-09-16 12:32:03 +0800

    move autohide jQuery into navbar.js

--- app/src/app/navbar/navbar.js
+++ app/src/app/navbar/navbar.js
@@ -6,6 +6,27 @@ angular.module('Dashboard.Navbar', [])
         replace: true,
         templateUrl: 'navbar/navbar.tpl.html',
         controller: function($scope, $rootScope, $location) {
+            // jQuery
+            showDistance = 50;
+            animateSpeed = 300 //ms
+
+            $(document).ready(function() {
+                $("#menu").hide();
+            });
+
+            function showNavBar() {
+                $("#menu").show(animateSpeed, 'linear');
+            };
+
+            $(document).mousemove(function(ev) {
+                var ev = ev || window.event;
+                if(ev.pageX <= showDistance) {
+                    showNavBar()
+                } else if (ev.pageX > showDistance) {
+                    $("#menu").hide(animateSpeed, 'linear');
+                }
+            });
+
             // Show / Hide Admin panel
             $scope.open = function() {
                 if($rootScope.showAdmin) {
