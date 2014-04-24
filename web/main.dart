fd23241b4f00972c4cb254189518b954c72e6480
bbss <baruchberger@gmail.com>
2014-04-24 18:35:11 +0900

    add applicationfactory

--- web/main.dart
+++ web/main.dart
@@ -3,6 +3,7 @@ library dashy;
 @MirrorsUsed(override: '*')
 import 'dart:mirrors';
 import 'package:angular/angular.dart';
+import 'package:angular/application_factory.dart';
 import 'package:dashy/client/dashy.dart';
 
 void main() {
