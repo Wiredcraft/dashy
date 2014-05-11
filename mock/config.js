c657857c73d34c295492202d5f0d2eaae99a52a6
Ronan Berder <ronan@wiredcraft.com>
2014-05-11 14:41:26 +0800

    Very early commit, getting started with mock

---
+++ mock/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "vendor": {
+        "js": [ ],
+        "css": [
+            "vendor/normalize-css/normalize.css"
+        ]
+    },
+    "custom": {
+        "js": [
+            "assets/js/**/*.js"
+        ],
+        "css": [
+            "assets/css/**/*.css"
+        ],
+        "scss": [
+            "assets/scss/**/*.{scss,sass}"
+        ]
+    }
+}
